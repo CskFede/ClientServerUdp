f367f6f4cdb3a516f9db8b9af42c1e21424f56b1
Federico Castrucci <federicoantonio.castrucci@gmail.com>
2023-03-01 22:24:09 +0100

    primo commit

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        // Creazione del socket di invio
+        DatagramSocket clientSocket = new DatagramSocket();
+        // Indirizzo IP del server
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = "Hello, server!";
+        sendData = sentence.getBytes();
+        // Creazione del pacchetto di invio
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        // 1. Invio del pacchetto unicast
+        clientSocket.send(sendPacket);
+        // 2. Ricezione della risposta
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("From Server: " + modifiedSentence);
+        // Chiusura del socket di invio
+        clientSocket.close();
+
+        // Creazione del socket di invio multicast
+        DatagramSocket multicastSocket = new DatagramSocket();
+        // Indirizzo IP del gruppo multicast
+        InetAddress groupAddress = InetAddress.getByName("230.0.0.1");
+        String multicastSentence = "Hello, multicast!";
+        sendData = multicastSentence.getBytes();
+        // Creazione del pacchetto di invio multicast
+        DatagramPacket multicastPacket = new DatagramPacket(sendData, sendData.length, groupAddress, 9876);
+        // 3. Invio del pacchetto multicast
+        multicastSocket.send(multicastPacket);
+        // Chiusura del socket di invio multicast
+        multicastSocket.close();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,26 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        // Creazione del socket di ricezione
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        while (true) {
+            // 1. Ricezione del pacchetto
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+            System.out.println("Received: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            // 2. Invio della risposta
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+}
